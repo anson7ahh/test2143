d788deddcfa5976abba1d39fedd2e38f654f6e1e
anson7ahh <nguyenanson54@gmail.com>
2025-08-08 13:14:02 +0700

    test4

--- app/(a)/a.tsx
+++ app/(a)/a.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function A() {
   return (
     <View>
-      <Text style={styles.headerImage}>aaaaabbbb313123bb</Text>
+      <Text style={styles.headerImage}>a</Text>
     </View>
   );
 }
