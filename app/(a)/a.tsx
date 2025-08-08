5a12d82c6f549cd40b88a4d3b67c66d4e962f447
anson7ahh <nguyenanson54@gmail.com>
2025-08-08 12:51:11 +0700

    test

---
+++ app/(a)/a.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function A() {
+  return (
+    <View>
+      <Text style={styles.headerImage}>aaaaa</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
